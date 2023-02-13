14ec999de1d0003bcc0ede9eb426117ce3a66932
Ian Hadrien <ianhadrien@gmail.com>
2023-02-12 21:54:48 -0300

    Responsividade

--- web/src/components/NavaBar/NavBar.jsx
+++ web/src/components/NavaBar/NavBar.jsx
@@ -2,9 +2,30 @@ import React, { useState } from 'react'
 import './NavBar.css';
 import Logo from '../../assets/img/logo.png';
 import { Link } from 'react-router-dom';
+import { Popover, Typography } from '@mui/material';
 
 export const NavBar = () => {
   const [name, setName] = useState(localStorage.getItem("name"));
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [menuMobile, setMenuMobile] = useState(0);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+
+    if(menuMobile === 0) {
+      setMenuMobile(1);
+    } else {
+      setMenuMobile(0);
+    }
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
 
   return (
     <div id='NavBar-Body'>
@@ -17,18 +38,39 @@ export const NavBar = () => {
         </label> */}
         <div className="left_area">
             {/* <h3>Go<span>Snippets</span></h3> */}
-            <img src={Logo} />
+            <img src={Logo} /> 
         </div>
-        <div className="NavBarright_area">
+        <div className="NavBarright_area" onClick={handleClick}>
             {/* <a href="#" className="logout_btn">Logout</a> */}
           <i className='bx bx-user-circle'></i>
           {name}
+          <i className='bx bx-chevron-down'></i>
         </div>
+        <Popover
+          id={id}
+          className="Popover-container"
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+        >
+          <Typography sx={{ p: 2 }}>
+            <Link className='Popover-Menu' to='/usuario'>
+              <span>Meu Perfil</span>
+            </Link> <br />
+            <Link className='Popover-Menu' to='/'>
+              <span>Sair</span>
+            </Link>
+          </Typography>
+        </Popover>
       </div>
 
       {/* <!--mobile navigation bar start--> */}
       <div className="mobile_nav">
-        <div className="mobile_nav_items">
+        <div className={`${menuMobile === 0 ? 'mobile_nav_items' : 'mobile_nav_items mobile_nav_items_MOBILE'}`}>
           <Link to='/dashbord'>
             <i className='bx bx-data'></i><span>Dashboard</span>
           </Link>
